bb064c2c0fbd2f7c45ed2c85d921d3d48d966a9b
Awang Hykal <hykalawg@gmail.com>
2024-11-25 18:44:54 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    alert('Thank you for your message. We will get back to you shortly.');
+  });
+  
