13e77c37d218c889e8b13575bd72cc6947300a79
Awang Hykal <hykalawg@gmail.com>
2024-11-26 08:17:01 +0800

    updated web page

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ document.getElementById('contact-form').addEventListener('submit', (event) => {
     event.preventDefault();
     alert('Thank you for your message. We will get back to you shortly.');
   });
-  
+  fetch('https://api.example.com/data')
+    .then(response => response.json())
+    .then(data => console.log(data));
